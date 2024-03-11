ac8eca57130b9f9edb78a956ed85ec94fb71836f
Ryan Packer <ryan.packer@medenterprises.com>
2024-03-11 15:13:31 +1300

    docs: add astronomer image

--- provider/resources.go
+++ provider/resources.go
@@ -105,7 +105,7 @@ func Provider() tfbridge.ProviderInfo {
 		//
 		// You may host a logo on a domain you control or add an SVG logo for your package
 		// in your repository and use the raw content URL for that file as your logo URL.
-		LogoURL: "https://raw.githubusercontent.com/ryan.pip/pulumi-astronomer/main/docs/astronomer.png",
+		LogoURL: "https://raw.githubusercontent.com/ryan.pip/pulumi-astronomer/main/docs/astronomer.svg",
 		// PluginDownloadURL is an optional URL used to download the Provider
 		// for use in Pulumi programs
 		// e.g https://github.com/org/pulumi-provider-name/releases/

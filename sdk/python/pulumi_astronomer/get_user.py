2859f34bb1c5bbe0b5505114ee7dea5e6b460773
ryan-pip <runner@fv-az802-259.15taxv3x2l5urdl5ou4x200swd.phxx.internal.cloudapp.net>
2024-10-18 05:10:05 +0000

    make build_sdks

--- sdk/python/pulumi_astronomer/get_user.py
+++ sdk/python/pulumi_astronomer/get_user.py
@@ -4,9 +4,14 @@
 
 import copy
 import warnings
+import sys
 import pulumi
 import pulumi.runtime
 from typing import Any, Mapping, Optional, Sequence, Union, overload
+if sys.version_info >= (3, 11):
+    from typing import NotRequired, TypedDict, TypeAlias
+else:
+    from typing_extensions import NotRequired, TypedDict, TypeAlias
 from . import _utilities
 from . import outputs
 
@@ -187,9 +192,6 @@ def get_user(id: Optional[str] = None,
         updated_at=pulumi.get(__ret__, 'updated_at'),
         username=pulumi.get(__ret__, 'username'),
         workspace_roles=pulumi.get(__ret__, 'workspace_roles'))
-
-
-@_utilities.lift_output_func(get_user)
 def get_user_output(id: Optional[pulumi.Input[str]] = None,
                     opts: Optional[pulumi.InvokeOptions] = None) -> pulumi.Output[GetUserResult]:
     """
@@ -208,4 +210,18 @@ def get_user_output(id: Optional[pulumi.Input[str]] = None,
 
     :param str id: User identifier
     """
-    ...
+    __args__ = dict()
+    __args__['id'] = id
+    opts = pulumi.InvokeOptions.merge(_utilities.get_invoke_opts_defaults(), opts)
+    __ret__ = pulumi.runtime.invoke_output('astronomer:index/getUser:getUser', __args__, opts=opts, typ=GetUserResult)
+    return __ret__.apply(lambda __response__: GetUserResult(
+        avatar_url=pulumi.get(__response__, 'avatar_url'),
+        created_at=pulumi.get(__response__, 'created_at'),
+        deployment_roles=pulumi.get(__response__, 'deployment_roles'),
+        full_name=pulumi.get(__response__, 'full_name'),
+        id=pulumi.get(__response__, 'id'),
+        organization_role=pulumi.get(__response__, 'organization_role'),
+        status=pulumi.get(__response__, 'status'),
+        updated_at=pulumi.get(__response__, 'updated_at'),
+        username=pulumi.get(__response__, 'username'),
+        workspace_roles=pulumi.get(__response__, 'workspace_roles')))
